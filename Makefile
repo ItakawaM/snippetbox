46f651c5b66bb9d5c1d17c01416f1d32f2bef4b3
ItakawaM <maksymworkp@gmail.com>
2026-06-02 23:18:13 +0300

    chore(Makefile): update .env parsing and server initialization

--- Makefile
+++ Makefile
@@ -3,16 +3,29 @@ include .env
 export
 
 BINARY ?= server.exe
+MAIN_PATH := ./cmd/web/
 
-.PHONY: run help
+.DEFAULT_GOAL := help
+
+.PHONY: run build build-run clean help
 
 run:
-	go run ./cmd/web/ -addr=$(HOST_ADDRESS) -dsn=$(POSTGRESQL_DSN)
+	go run $(MAIN_PATH) -addr=$(HOST_ADDRESS) -dsn=$(POSTGRESQL_DSN)
 
 build:
-	go build -o $(BINARY) ./cmd/web/ -addr=$(HOST_ADDRESS)
+	go build -o $(BINARY) $(MAIN_PATH)
+
+build-run: build
+	./$(BINARY) -addr=$(HOST_ADDRESS) -dsn=$(POSTGRESQL_DSN)
+
+clean:
+	@echo "Cleaning up..."
+	@rm -f $(BINARY)
+	@echo "Done!"
 
 help:
 	@echo "Commands:"
-	@echo "  make run     - Runs the Web Server"
-	@echo "  make build   - Builds the Web Server into an executable"
+	@echo "  make run         - Runs the Web Server"
+	@echo "  make build       - Builds the Web Server into an executable"
+	@echo "  make build-run   - Runs the Web Server executable"
+	@echo "  make clean       - Removes the compiled binary"
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,19 +7,44 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/ItakawaM/snippetbox/internal/models"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 type application struct {
 	infoLogger  *log.Logger
 	errorLogger *log.Logger
+	snippets    *models.SnippetModel
 }
 
-func newApplication() *application {
-	return &application{
-		infoLogger:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
-		errorLogger: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile),
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	dsn := flag.String("dsn", "", "PostreSQL Data Source Name")
+	flag.Parse()
+
+	infoLogger := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLogger := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLogger.Fatal(err)
 	}
+	defer db.Close()
+
+	app := &application{
+		infoLogger:  infoLogger,
+		errorLogger: errorLogger,
+		snippets:    &models.SnippetModel{DB: db},
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		Handler:  app.routes(),
+		ErrorLog: app.errorLogger,
+	}
+
+	infoLogger.Printf("Starting server on %s", *addr)
+	errorLogger.Fatal(srv.ListenAndServe())
 }
 
 func openDB(dsn string) (*sql.DB, error) {
@@ -33,26 +58,4 @@ func openDB(dsn string) (*sql.DB, error) {
 	}
 
 	return db, err
-}
-
-func main() {
-	addr := flag.String("addr", ":4000", "HTTP Network Address")
-	dsn := flag.String("dsn", "", "PostreSQL Data Source Name")
-	flag.Parse()
-
-	app := newApplication()
-	db, err := openDB(*dsn)
-	if err != nil {
-		app.errorLogger.Fatal(err)
-	}
-	defer db.Close()
-
-	srv := &http.Server{
-		Addr:     *addr,
-		Handler:  app.routes(),
-		ErrorLog: app.errorLogger,
-	}
-
-	app.infoLogger.Printf("Starting server on %s", *addr)
-	app.errorLogger.Fatal(srv.ListenAndServe())
 }
