2f58a1e702efc0360a6ddd4cd6203d34cfd76cbd
ItakawaM <maksymworkp@gmail.com>
2026-06-14 22:01:00 +0300

    feat(main): implement user logout

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -207,5 +207,13 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+	if err := app.sessionManager.RenewToken(r.Context()); err != nil {
+		app.serverError(w, err)
+		return
+	}
 
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+	app.sessionManager.Put(r.Context(), "flash", "Your logout was successful.")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
