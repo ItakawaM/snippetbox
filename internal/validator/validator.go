646dda48dedd15296e02ab85a28d5840b00f8ad7
ItakawaM <maksymworkp@gmail.com>
2026-06-14 21:44:19 +0300

    feat(validator): add methods to verify user data

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,16 +1,22 @@
 package validator
 
 import (
+	"regexp"
+	"slices"
 	"strings"
+	"unicode"
 	"unicode/utf8"
 )
 
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
 type Validator struct {
-	FieldErrors map[string]string
+	NonFieldErrors []string
+	FieldErrors    map[string]string
 }
 
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
 }
 
 func (v *Validator) AddFieldError(key string, message string) {
@@ -21,6 +27,10 @@ func (v *Validator) AddFieldError(key string, message string) {
 	if _, exists := v.FieldErrors[key]; !exists {
 		v.FieldErrors[key] = message
 	}
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
 
 func (v *Validator) CheckField(ok bool, key string, message string) {
@@ -37,12 +47,37 @@ func MaxChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) <= n
 }
 
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
 func PermitttedInt(value int, permittedValues ...int) bool {
-	for i := range permittedValues {
-		if value == permittedValues[i] {
-			return true
+	return slices.Contains(permittedValues, value)
+}
+
+func Matches(value string, expression *regexp.Regexp) bool {
+	return expression.MatchString(value)
+}
+
+func ValidPassword(value string) bool {
+	if !MinChars(value, 8) {
+		return false
+	}
+
+	var hasUpper, hasLower, hasNumber, hasSpecial bool
+
+	for _, char := range value {
+		switch {
+		case unicode.IsUpper(char):
+			hasUpper = true
+		case unicode.IsLower(char):
+			hasLower = true
+		case unicode.IsDigit(char):
+			hasNumber = true
+		case strings.ContainsRune("#?!@$%^&*-", char):
+			hasSpecial = true
 		}
 	}
 
-	return false
+	return hasUpper && hasLower && hasNumber && hasSpecial
 }
