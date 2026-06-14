5ae67e127aa672ebbd9314809ff32c58ef1c4dca
ItakawaM <maksymworkp@gmail.com>
2026-06-14 21:44:54 +0300

    feat(models): add errors for user model

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -3,3 +3,6 @@ package models
 import "errors"
 
 var ErrNoRecord error = errors.New("models: no matching record found")
+
+var ErrInvalidCredentials error = errors.New("models: invalid credentials")
+var ErrDuplicateEmail error = errors.New("models: duplicate email")
