b936282141191fc4667f67d1b5c6463f95daa99d
ItakawaM <maksymworkp@gmail.com>
2026-06-15 12:31:56 +0300

    feat(main): implement additional csrf protection

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -47,7 +47,7 @@ func (app *application) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
