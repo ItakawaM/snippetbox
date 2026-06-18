30013da56cba7d6f7103b2c3bdf1f4a27650485d
ItakawaM <maksymworkp@gmail.com>
2026-06-18 17:42:37 +0300

    feat(models): setup test db for integration testing

---
+++ internal/models/testutils_test.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func newTestDB(t *testing.T) *sql.DB {
+	db, err := sql.Open("pgx", "postgres://test_snippetbox:test_snippetbox@localhost:5432/test_snippetbox_db")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	script, err := os.ReadFile("./testdata/setup.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = db.Exec(string(script))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		script, err := os.ReadFile("./testdata/teardown.sql")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = db.Exec(string(script))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		db.Close()
+	})
+
+	return db
+}
