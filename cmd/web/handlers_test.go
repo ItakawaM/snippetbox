a3e456d18a4f368acfcabb048f82e3d1d4415ba1
ItakawaM <maksymworkp@gmail.com>
2026-06-19 14:42:29 +0300

    test(main): add snippetCreate end-to-end testing

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -216,3 +216,31 @@ func TestUserSignup(t *testing.T) {
 		})
 	}
 }
+
+func TestApplication_SnippetCreate(t *testing.T) {
+	app := newTestApplcation(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	t.Run("Unauthenticated", func(t *testing.T) {
+		code, header, _ := ts.get(t, "/snippet/create")
+
+		assert.Equal(t, code, http.StatusSeeOther)
+		assert.Equal(t, header.Get("Location"), "/user/login")
+	})
+
+	t.Run("Authenticated", func(t *testing.T) {
+		_, _, body := ts.get(t, "/user/login")
+		csrf_token := extractCSRFToken(t, body)
+
+		form := url.Values{}
+		form.Add("email", "correct@example.com")
+		form.Add("password", "pa$$Word1")
+		form.Add("csrf_token", csrf_token)
+		ts.postForm(t, "/user/login", form)
+
+		code, _, body := ts.get(t, "/snippet/create")
+		assert.Equal(t, code, http.StatusOK)
+		assert.StringContains(t, body, `<form action="/snippet/create" method="POST">`)
+	})
+}
